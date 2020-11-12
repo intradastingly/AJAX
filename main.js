6cd975f52af10993231466f57cc2a5ea8a3b3825
Asaf Läckgren <asaf_lackgren@hotmail.com>
2020-11-12 14:02:29 +0100

    html

--- main.js
+++ main.js
@@ -8,4 +8,10 @@ function addEventListeners() {
     document.getElementById('ajax-button').addEventListener('click', fetchDaysWithAjaxAsyncAwait);
     document.getElementById('fetch-button').addEventListener('click', fetchDaysWithFetch);
     document.getElementById('fetch-promise').addEventListener('click', fetchDaysWithFetchPromise);
+    document.getElementById('html-button').addEventListener('click', fetchHtmlDocument);
+}
+
+async function fetchHtmlDocument() {
+    const response = await fetch('./index.html');
+    console.log(await response.text());
 }
